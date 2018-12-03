c7852ad6d15eb7c875608071ba824fefa93e5e3e
yjh1227 <1254094880@qq.com>
2018-12-03 18:26:27 +0800

    modify

--- domitory.sql
+++ domitory.sql
@@ -1,18 +1,4 @@
-/*
-SQLyog v10.2 
-MySQL - 5.5.47 : Database - domitory
-*********************************************************************
-*/
 
-
-/*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 CREATE DATABASE /*!32312 IF NOT EXISTS*/`domitory` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
 USE `domitory`;
